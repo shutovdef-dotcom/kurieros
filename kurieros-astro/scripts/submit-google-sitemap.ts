0f2bde279740037f61056898817565568d6a2f3d
Ivan <ivan@MacBook-Pro-Ivan.local>
2026-08-01 16:22:41 +0300

    ci: use URL-prefix property for Google sitemap submit

--- kurieros-astro/scripts/submit-google-sitemap.ts
+++ kurieros-astro/scripts/submit-google-sitemap.ts
@@ -25,7 +25,7 @@ type SitemapSubmitResult = {
 
 const rootDir = resolve(dirname(new URL(import.meta.url).pathname), '..');
 const DEFAULT_SITEMAP_URL = 'https://kurerok.ru/sitemap-index.xml';
-const DEFAULT_SITE_URL = 'sc-domain:kurerok.ru';
+const DEFAULT_SITE_URL = 'https://kurerok.ru/';
 const DEFAULT_ENDPOINT_BASE = 'https://www.googleapis.com/webmasters/v3/sites';
 
 const readOption = (name: string): string | undefined => {
